40dd1d5b347fcf9871a14a251771965e24f40939
edxnielwork <edxnielwork007@gmail.com>
2022-03-15 23:31:05 -0500

    UPDATED

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LLAVERO APP',
+      title: 'THE ACADEMY',
       debugShowCheckedModeBanner: false,
       initialRoute: 'login',
       routes: {
